e24770b77763a0213dd2ca3aa103d733a1684859
IPOMPONI <pedantic8.8programmer@gmail.com>
2025-10-18 21:56:16 +0300

    feat: added insert and get all books

---
+++ internal/bookstorage/book_db.go
@@ -0,0 +1,49 @@
+package bookstorage
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+)
+
+func InsertBook(conn *pgx.Conn, book Book) error {
+	query := `INSERT INTO Book  (title, author, year_published) VALUES ($1, $2, $3)`
+
+	_, err := conn.Exec(context.Background(), query, book.Title, book.Author, book.YearPublished)
+
+	return err
+}
+
+func GetAllBooksData(conn *pgx.Conn) ([]Book, error) {
+	query := `SELECT id, title, author, year_published, added_at FROM Book ORDER BY id`
+
+	rows, err := conn.Query(context.Background(), query)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var bookAllData []Book
+
+	for rows.Next() {
+		var book Book
+
+		err := rows.Scan(
+			&book.Id,
+			&book.Title,
+			&book.Author,
+			&book.YearPublished,
+			&book.AddedAt,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+
+		bookAllData = append(bookAllData, book)
+	}
+
+	return bookAllData, rows.Err()
+}
