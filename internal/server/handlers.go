99a20f78bad0fb3d2fae5b702726e26594561b80
IPOMPONI <pedantic8.8programmer@gmail.com>
2025-11-05 14:46:20 +0300

    refactor: internal funcs router have become private

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -19,15 +19,15 @@ func NewBookHandler(repo repository.BookRepo) *BookHandler {
 }
 
 func (bh *BookHandler) InitRoutes(mux *http.ServeMux) {
-	mux.HandleFunc("POST /books", bh.InsertBookHandler)
-	mux.HandleFunc("GET /books/{id}", bh.GetBookByIdHandler)
-	mux.HandleFunc("GET /books", bh.GetAllBooksHandler)
-	mux.HandleFunc("PUT /books/{id}", bh.UpdateBookByIdHandler)
-	mux.HandleFunc("DELETE /books/{id}", bh.DeleteBookByIdHandler)
-	mux.HandleFunc("DELETE /books", bh.DeleteAllBooksHandler)
+	mux.HandleFunc("POST /books", bh.insertBook)
+	mux.HandleFunc("GET /books/{id}", bh.getBookById)
+	mux.HandleFunc("GET /books", bh.getAllBooks)
+	mux.HandleFunc("PUT /books/{id}", bh.updateBookById)
+	mux.HandleFunc("DELETE /books/{id}", bh.deleteBookById)
+	mux.HandleFunc("DELETE /books", bh.deleteAllBooks)
 }
 
-func (bh *BookHandler) InsertBookHandler(w http.ResponseWriter, r *http.Request) {
+func (bh *BookHandler) insertBook(w http.ResponseWriter, r *http.Request) {
 	var book model.Book
 
 	if err := json.NewDecoder(r.Body).Decode(&book); err != nil {
@@ -44,7 +44,7 @@ func (bh *BookHandler) InsertBookHandler(w http.ResponseWriter, r *http.Request)
 	fmt.Fprintf(w, `{"message": "Book created"}`)
 }
 
-func (bh *BookHandler) GetBookByIdHandler(w http.ResponseWriter, r *http.Request) {
+func (bh *BookHandler) getBookById(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.PathValue("id"))
 
 	if err != nil {
@@ -63,7 +63,7 @@ func (bh *BookHandler) GetBookByIdHandler(w http.ResponseWriter, r *http.Request
 	json.NewEncoder(w).Encode(book)
 }
 
-func (bh *BookHandler) GetAllBooksHandler(w http.ResponseWriter, r *http.Request) {
+func (bh *BookHandler) getAllBooks(w http.ResponseWriter, r *http.Request) {
 	books, err := bh.repo.GetAllBooks(r.Context())
 
 	if err != nil {
@@ -75,7 +75,7 @@ func (bh *BookHandler) GetAllBooksHandler(w http.ResponseWriter, r *http.Request
 	json.NewEncoder(w).Encode(books)
 }
 
-func (bh *BookHandler) UpdateBookByIdHandler(w http.ResponseWriter, r *http.Request) {
+func (bh *BookHandler) updateBookById(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.PathValue("id"))
 
 	if err != nil {
@@ -103,7 +103,7 @@ func (bh *BookHandler) UpdateBookByIdHandler(w http.ResponseWriter, r *http.Requ
 	fmt.Fprintf(w, `{"message": "Book with id = %d was updated"}`, id)
 }
 
-func (bh *BookHandler) DeleteBookByIdHandler(w http.ResponseWriter, r *http.Request) {
+func (bh *BookHandler) deleteBookById(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.PathValue("id"))
 
 	if err != nil {
@@ -121,7 +121,7 @@ func (bh *BookHandler) DeleteBookByIdHandler(w http.ResponseWriter, r *http.Requ
 	fmt.Fprintf(w, `{"message": "Book with id = %v was deleted"}`, id)
 }
 
-func (bh *BookHandler) DeleteAllBooksHandler(w http.ResponseWriter, r *http.Request) {
+func (bh *BookHandler) deleteAllBooks(w http.ResponseWriter, r *http.Request) {
 
 	err := bh.repo.DeleteAllBooks(r.Context())
 
